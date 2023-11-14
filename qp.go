e6575756a30237f07abece29fc8da0eb4e972ae6
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-11-14 12:41:16 +0100

    Add files via upload

---
+++ qp.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	qp "mime/quotedprintable"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: qp [-d -e]")
+		os.Exit(1)
+	}
+
+	opt := os.Args[1]
+
+	reader := bufio.NewReader(os.Stdin)
+
+	switch opt {
+	case "-d":
+		for {
+			input, err := reader.ReadString('\n')
+			if err == io.EOF {
+				break
+			}
+			decoded := qp.NewReader(strings.NewReader(input))
+			buf := new(strings.Builder)
+			io.Copy(buf, decoded)
+			fmt.Print(buf.String())
+		}
+	case "-e":
+		for {
+			input, err := reader.ReadString('\n')
+			if err == io.EOF {
+				break
+			}
+			encoded := qp.NewWriter(os.Stdout)
+			encoded.Write([]byte(input))
+			encoded.Close()
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Usage: qp [-d -e]")
+		os.Exit(1)
+	}
+}
+
